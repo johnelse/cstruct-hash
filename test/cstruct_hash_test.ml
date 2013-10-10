2a39d28299448fdd95b082e469f872ccfc3eb019
John Else <john.else@citrix.com>
2013-10-10 15:37:04 +0100

    Add non-zero offset tests
    Signed-off-by: John Else <john.else@citrix.com>

--- test/cstruct_hash_test.ml
+++ test/cstruct_hash_test.ml
@@ -1,12 +1,13 @@
 open OUnit
 
-let test_strings = [
-	"";
-	"foo";
+let non_empty_test_strings = [
+	"foobar";
 	"as89f7d8f798d7f987f0daf";
 	";£*%($)(^$&";
-	"\\\n\\";
+	"\\\n\\xyz";
 ]
+
+let all_test_strings = "" :: non_empty_test_strings
 
 let test_hash_all data =
 	let cstruct = Cstruct.of_string data in
@@ -24,12 +25,33 @@ let suite_test_hash_all =
 						(String.escaped test_string)
 				in
 				test_name >:: (fun () -> test_hash_all test_string))
-			test_strings)
+			all_test_strings)
+
+let test_hash_part data =
+	let offset = 1 in
+	let length = 4 in
+	let cstruct = Cstruct.of_string data in
+	let string_digest = Digest.string (String.sub data offset length) in
+	let cstruct_digest = Cstruct_hash.md5sum cstruct offset length in
+	assert_equal string_digest cstruct_digest
+
+let suite_test_hash_part =
+	"suite_test_hash_part" >:::
+		(List.map
+			(fun test_string ->
+				let test_name =
+					Printf.sprintf
+						"test_hash_part: \"%s\""
+						(String.escaped test_string)
+				in
+				test_name >:: (fun () -> test_hash_part test_string))
+			non_empty_test_strings)
 
 let base_suite =
 	"base_suite" >:::
 		[
 			suite_test_hash_all;
+			suite_test_hash_part;
 		]
 
 let _ = run_test_tt_main base_suite

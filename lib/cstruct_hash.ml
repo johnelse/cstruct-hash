261aa9b2e610ba1a92d069d964c9864c53ec71aa
John Else <john.else@citrix.com>
2013-10-10 14:09:16 +0100

    Return the raw digest instead of converting to hex
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/cstruct_hash.ml
+++ lib/cstruct_hash.ml
@@ -1,5 +1,4 @@
 external unsafe_md5sum : Cstruct.buffer -> int -> int -> string = "cstruct_md5sum"
 
 let md5sum t offset length =
-	let digest = unsafe_md5sum t.Cstruct.buffer (offset + t.Cstruct.off) length in
-	Digest.to_hex digest
+	unsafe_md5sum t.Cstruct.buffer (offset + t.Cstruct.off) length
--- test/cstruct_hash_test.ml
+++ test/cstruct_hash_test.ml
@@ -2,7 +2,7 @@ let data = "as89f7d8f798d7f987f0daf"
 
 let () =
 	let cstruct = Cstruct.of_string data in
-	let string_hash = Digest.to_hex (Digest.string data) in
-	let cstruct_hash = Cstruct_hash.md5sum cstruct 0 (String.length data) in
-	if string_hash <> cstruct_hash
-	then failwith (Printf.sprintf "not equal: %s %s" string_hash cstruct_hash)
+	let string_digest = Digest.string data in
+	let cstruct_digest = Cstruct_hash.md5sum cstruct 0 (String.length data) in
+	if string_digest <> cstruct_digest
+	then failwith (Printf.sprintf "not equal: %s %s" string_digest cstruct_digest)

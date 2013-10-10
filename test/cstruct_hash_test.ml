e55fd47d8d62ff3ce2d2008f9ecc7c9becf1ccb2
John Else <john.else@citrix.com>
2013-10-10 14:58:34 +0100

    Add oUnit and more tests
    Signed-off-by: John Else <john.else@citrix.com>

--- test/cstruct_hash_test.ml
+++ test/cstruct_hash_test.ml
@@ -1,8 +1,35 @@
-let data = "as89f7d8f798d7f987f0daf"
+open OUnit
 
-let () =
+let test_strings = [
+	"";
+	"foo";
+	"as89f7d8f798d7f987f0daf";
+	";£*%($)(^$&";
+	"\\\n\\";
+]
+
+let test data =
 	let cstruct = Cstruct.of_string data in
 	let string_digest = Digest.string data in
 	let cstruct_digest = Cstruct_hash.md5sum cstruct 0 (String.length data) in
-	if string_digest <> cstruct_digest
-	then failwith (Printf.sprintf "not equal: %s %s" string_digest cstruct_digest)
+	assert_equal string_digest cstruct_digest
+
+let test_hash_all =
+	"test_hash_all" >:::
+		(List.map
+			(fun test_string ->
+				let test_name =
+					Printf.sprintf
+						"test_hash_all: \"%s\""
+						(String.escaped test_string)
+				in
+				test_name >:: (fun () -> test test_string))
+			test_strings)
+
+let base_suite =
+	"base_suite" >:::
+		[
+			test_hash_all;
+		]
+
+let _ = run_test_tt_main base_suite

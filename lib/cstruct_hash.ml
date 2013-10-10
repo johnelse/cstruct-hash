2a1a38b7754216194f4b05d8356699561c3d0413
John Else <john.else@citrix.com>
2013-10-10 16:32:50 +0100

    Add bounds checking
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/cstruct_hash.ml
+++ lib/cstruct_hash.ml
@@ -1,4 +1,9 @@
 external unsafe_md5sum : Cstruct.buffer -> int -> int -> string = "cstruct_md5sum"
 
 let md5sum t offset length =
-	unsafe_md5sum t.Cstruct.buffer (offset + t.Cstruct.off) length
+	if offset > t.Cstruct.len then invalid_arg "offset";
+	if length < 0 then invalid_arg "length";
+	if offset + length > t.Cstruct.len then invalid_arg "length";
+	let offset_internal = offset + t.Cstruct.off in
+	if offset_internal < 0 then invalid_arg "offset";
+	unsafe_md5sum t.Cstruct.buffer offset_internal length

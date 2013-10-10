13c5972829a4485af8d3711209601fc40d62a36d
John Else <john.else@citrix.com>
2013-10-10 15:01:31 +0100

    Rename some variables
    Signed-off-by: John Else <john.else@citrix.com>

--- test/cstruct_hash_test.ml
+++ test/cstruct_hash_test.ml
@@ -8,14 +8,14 @@ let test_strings = [
 	"\\\n\\";
 ]
 
-let test data =
+let test_hash_all data =
 	let cstruct = Cstruct.of_string data in
 	let string_digest = Digest.string data in
 	let cstruct_digest = Cstruct_hash.md5sum cstruct 0 (String.length data) in
 	assert_equal string_digest cstruct_digest
 
-let test_hash_all =
-	"test_hash_all" >:::
+let suite_test_hash_all =
+	"suite_test_hash_all" >:::
 		(List.map
 			(fun test_string ->
 				let test_name =
@@ -23,13 +23,13 @@ let test_hash_all =
 						"test_hash_all: \"%s\""
 						(String.escaped test_string)
 				in
-				test_name >:: (fun () -> test test_string))
+				test_name >:: (fun () -> test_hash_all test_string))
 			test_strings)
 
 let base_suite =
 	"base_suite" >:::
 		[
-			test_hash_all;
+			suite_test_hash_all;
 		]
 
 let _ = run_test_tt_main base_suite
